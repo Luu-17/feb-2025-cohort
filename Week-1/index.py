c74247c4a2e9aa36aaa545badf9fe9fb996fccaf
Luu-17 <groups.shoulin@gmail.com>
2025-03-11 11:05:32 +0200

    Intro to python c2

--- Week-1/index.py
+++ Week-1/index.py
@@ -1,1 +1,20 @@
 print("Hello World")
+
+number = 10
+print(number)
+
+age = 27 #int DataType
+mark = 90.5 #float DataType
+name = "Lukhanyo Mzanywa" #Sting DataType
+languages = ["python", "HTML", "CSS", 3, 6.4, True]
+print(languages)
+print("left to Right")
+print(languages[0]) # When printing from left to right you star with 0
+print(languages[2])
+print(languages[5])
+print('right to left')
+print(languages[-4])
+print(languages[-6])
+print(languages[-1]) # When printing from right to left you start with -1
+
+
